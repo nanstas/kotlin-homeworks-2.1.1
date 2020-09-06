0b54171efc7123bd610075434e704c2d05da8b7d
Stanislav Nanoshkin <nanstas@yandex.ru>
2020-09-06 10:21:09 +0500

    made the magic numbers into variables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,13 +12,15 @@ fun main() {
 fun calculationCommission(typeCard: String = "VK Pay", previousTransaction: Int = 0, transferAmount: Int): Int {
     val prefLimit = 75_000_00
     val overrunLimit = prefLimit - previousTransaction - transferAmount
-    val commission = transferAmount * 0.0075
-    val minCommission1 = 20 * 100
-    val minCommission2 = 35 * 100
+    val minCommissionForMastercardMaestro = 20 * 100
+    val commissionForMastercardMaestro = 0.006
+    val minCommissionForVisaMir = 35 * 100
+    val commissionForVisaMir = 0.0075
+    val totalCommissionForVisaMir = transferAmount * commissionForVisaMir
 
     return when(typeCard) {
-        "Mastercard", "Maestro" -> if (overrunLimit < 0) (kotlin.math.abs(overrunLimit) * 0.006 + minCommission1).toInt() else 0
-        "Visa", "Мир" ->  if (commission > minCommission2) commission.toInt() else minCommission2
+        "Mastercard", "Maestro" -> if (overrunLimit < 0) (kotlin.math.abs(overrunLimit) * commissionForMastercardMaestro + minCommissionForMastercardMaestro).toInt() else 0
+        "Visa", "Мир" ->  if (totalCommissionForVisaMir > minCommissionForVisaMir) totalCommissionForVisaMir.toInt() else minCommissionForVisaMir
         else -> 0
     }
 }
